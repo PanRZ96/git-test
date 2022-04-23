b54a5457e857c7041ad122ad7e2f36671ab55943
prz96 <49935652+PanRZ96@users.noreply.github.com>
2022-04-23 15:09:25 +0800

    Update GitTestTwo.java

--- src/main/java/com/atguigu/git/GitTestTwo.java
+++ src/main/java/com/atguigu/git/GitTestTwo.java
@@ -12,5 +12,8 @@ public class GitTestTwo {
         System.out.println("prz7777777");
         System.out.println("prz88888888");
         System.out.println("push test");
+        System.out.println("====================");
+        System.out.println("clone test");
+     
     }
 }
