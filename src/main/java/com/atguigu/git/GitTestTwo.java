ff6209191e91ea2d66b44e942478978d792ecbce
prz96 <1633704272@qq.com>
2022-04-23 13:30:34 +0800

    prz 2 merge master commit

--- src/main/java/com/atguigu/git/GitTestTwo.java
+++ src/main/java/com/atguigu/git/GitTestTwo.java
@@ -2,7 +2,7 @@ package com.atguigu.git;
 
 public class GitTestTwo {
     public static void main(String[] args) {
-        System.out.println("prz11");
+        System.out.println("prz1100000");
         System.out.println("prz22");
         System.out.println("prz33");
     }
